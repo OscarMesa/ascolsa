6b6ffa40e2528bc2c6b7ac8a3c24396378e5fb6c
oscar <oscarmesa.elpoli@gmail.com>
2013-12-06 10:13:30 -0500

    error google analytics

--- templates/plantillapornettic/index.php
+++ templates/plantillapornettic/index.php
@@ -115,16 +115,6 @@ if ($menu->getActive() == $menu->getDefault()) {
 	. ($itemid ? ' itemid-' . $itemid : '')
 	. ($params->get('fluidContainer') ? ' fluid' : '');
 ?>">
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-44981772-1', 'escueladevida.com.co');
-  ga('send', 'pageview');
-
-</script>
 <?php
 $app = JFactory::getApplication();
 $menu = $app->getMenu();
